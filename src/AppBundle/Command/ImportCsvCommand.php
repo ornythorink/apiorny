5121f3ec7fd62da64ff90d74f411ccd93146f974
Ornythorink <orny2007@gmail.com>
2017-08-26 10:52:00 +0200

    methode tdd dans import commande

--- src/AppBundle/Command/ImportCsvCommand.php
+++ src/AppBundle/Command/ImportCsvCommand.php
@@ -82,7 +82,7 @@ class ImportCsvCommand extends ContainerAwareCommand
 
         $data = $this->getDataFromCsvExtractor();
         $method = $this->source;
-
+        var_dump($data);
         $this->loadBlacklist();
         $this->loadWhitelist();
         $this->$method($data);
@@ -275,4 +275,60 @@ SQL;
         }
 
     }
+
+    public function tdd($data)
+    {
+        $rawquery = <<<SQL
+            INSERT INTO `shoes`.`products`
+                    (  `name`, `price`, `promo`,`url`, `short_url`, `currency`,`logostore`,
+                     `program`, `status`,  `brand`, `image`, `source_id`, `source_type`,  `actif`, `locale`,
+                      `category_merchant`,  `createdAt`, `updateAt`, `description`, `ean` )
+                    VALUES  (
+                     :name,  :price, :promo ,:url, :short_url, :currency, :logostore,
+                    :program , :status, :brand ,:image, :source_id, :source_type, :actif,
+                    :locale, :category_merchant, :createdAt, :updateAt, :description,
+                    :ean
+                    )
+                    ON DUPLICATE KEY UPDATE updateAt = :now;
+SQL;
+        $statement = $this->em->getConnection()->prepare($rawquery);
+        $now = new \DateTime('now');
+            foreach($data as $row)
+            {
+                if(isset( $this->blacklist[$row['merchantCategoryName']] ))
+                {
+                    // @todo increment hit sur la blackliste
+                } else {  // si la categorie n'est pas dans la blacklist
+
+                    if ($row['previousPrice'] == "")
+                    {
+                        $row['previousPrice'] = 0.00;
+                    }
+
+                    $statement->bindValue('name', $row['name']	);
+                    $statement->bindValue('price', $row['price']);
+                    $statement->bindValue('promo', $row['previousPrice']);
+                    $statement->bindValue('url', $row['productUrl'] );
+                    $statement->bindValue('short_url', MD5($row['productUrl']) );
+                    $statement->bindValue('currency',  $row['currency']);
+                    $statement->bindValue('logostore', $row['programLogoPath']);
+                    $statement->bindValue('program', $row['programName'] );
+                    $statement->bindValue('status', "Validation" );
+                    $statement->bindValue('brand', $row['brand'] );
+                    $statement->bindValue('image', $row['imageUrl']);
+                    $statement->bindValue('source_id', 'TDD' );
+                    $statement->bindValue('source_type', 'CSV');
+                    $statement->bindValue('actif', 'Y');
+                    $statement->bindValue('locale', $this->locale);
+                    $statement->bindValue('category_merchant', $row['merchantCategoryName'] );
+                    $statement->bindValue('createdAt', $now->format('Y-m-d H:i:s'));
+                    $statement->bindValue('updateAt', $now->format('Y-m-d H:i:s'));
+                    $statement->bindValue('description', $row['description'] );
+                    $statement->bindValue('ean', $row['ean']  );
+                    $statement->bindValue('now', $now->format('Y-m-d H:i:s'));
+                    $statement->execute();
+                    }
+            }
+
+        }
 }
